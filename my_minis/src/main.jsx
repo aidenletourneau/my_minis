c04bcde4896c5aebcd68b27ef5a32e93ebcfd641
Aiden Letourneau <aidenletourneau@gmail.com>
2024-09-06 10:54:39 -0400

    initial commit

---
+++ my_minis/src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import * as ReactDOM from "react-dom/client"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css"
+import Home from './routes/Home.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>,
+  },
+
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>
+)
